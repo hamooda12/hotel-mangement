61a95fe0e78253096ae9a8d30be1affb42dd6556
Hamad tarawa <tourt2031@gmail.com>
2026-09-05 14:08:38 +0300

    feat: mount AI assistant in application layout

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,13 +1,15 @@
 import { Header } from "./Header";
 import { Outlet } from "react-router-dom";
-import '../FunctionsofTheProject/toast.css'
+import "../FunctionsofTheProject/toast.css";
+import { AiAssistant } from "../HelpersComponnent/AiAssistant";
+
 export function Layout() {
   return (
     <>
-         
       <Header />
       <Outlet />
-       <div id="toast-container" className="toast-container"></div>
+      <AiAssistant />
+      <div id="toast-container" className="toast-container"></div>
     </>
   );
 }
