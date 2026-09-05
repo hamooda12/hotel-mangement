db0cf0cf83821471263ea44eebd9db6afbf19bc6
Hamad tarawa <tourt2031@gmail.com>
2026-09-05 13:24:10 +0300

    Handle refresh token rotation consistently

--- src/FunctionsofTheProject/RefrechToken.js
+++ src/FunctionsofTheProject/RefrechToken.js
@@ -1,11 +1,28 @@
 import axios from "axios";
+
 const API = "https://hotel-management-monolith-backend.onrender.com/api/auth";
+
 export const refreshToken = async () => {
+  const storedRefreshToken = localStorage.getItem("refreshToken");
+
+  if (!storedRefreshToken) {
+    throw new Error("No refresh token available");
+  }
+
   const res = await axios.post(`${API}/refresh`, {
-    refreshToken: localStorage.getItem("refreshToken"),
+    refreshToken: storedRefreshToken,
   });
 
+  if (!res.data.accessToken) {
+    throw new Error("Refresh response did not contain an access token");
+  }
+
   localStorage.setItem("accessToken", res.data.accessToken);
+
+  // Keep the new refresh token when the backend rotates it.
+  if (res.data.refreshToken) {
+    localStorage.setItem("refreshToken", res.data.refreshToken);
+  }
 
   return res.data;
 };
